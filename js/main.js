5d727e2e2daa367ed344de085983d16bebdd3d99
Ruziokhunov4115956 <abdulazizruziokhunov4115956@gmail.com>
2022-08-24 13:39:43 +0500

    Misollar

---
+++ js/main.js
@@ -0,0 +1,50 @@
+//==================== FUNCTION ORQALI JUFT VA TOQ SON CHIQARUVCHI MISOL ====================//
+// function ishla(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (i % 2 !== 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// ishla(10)
+// ============================================================================================= //
+
+
+//=== FUNCTION ORQALI JUFT VA TOQ SONLARINING BIR BIRIGA QO'SHILGAN QIMATNI HISIBLOVCHI MISOL ===//
+// function ishla(num) {
+//     let caunt = 0
+//     for (let a = 1; a <= num; a++) {
+//         if (a % 2 !== 0) {
+//             caunt = caunt + a
+//         }
+//     }
+//     console.log(caunt);
+// }
+// ishla(10)
+// ============================================================================================== //
+
+ 
+//========== FUNCTION ORQALI FACTORIAL SONNI CHIQARUVCHI MISOL( 1 * 2 * 3 * 4 * 5 = 120) =========//
+// function factorial(num) {
+//     let caunt = 1
+//     for (let a = 1; a <= num; a++) {
+//         caunt = caunt * a
+//     }
+//     console.log(caunt);
+// }
+// factorial(5)
+ 
+//======================================= BOSHQACHA KO'RINISHI ==================================//
+// let factorial = function fac(n) { return n < 2 ? 1 : n * fac(n - 1); };
+// console.log(factorial(5));
+// ============================================================================================= //
+
+
+// ============================== FUNCTION ORQALI KARRA CHIQARISH ===============================//
+// let times = (num) => 7 * num
+// for (let i = 1; i <= 10; i++) console.log(times(i));
+
+//======================================= BOSHQACHA KO'RINISHI ==================================//
+// function ishla(num) { for (let i = 1; i <= 10; i++) { console.log(`7 x ${i} = ${7 * i}`) } }ishla(5)
+// ============================================================================================= //
+
